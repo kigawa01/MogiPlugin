eaf7b4524a7609e9ba320d1c52255eeadbf74c5a
kigawa <kigawa.8390@gmail.com>
2021-05-09 01:34:45 +0900

    Initial commit

---
+++ src/main/java/net/kigawa/minecraft/spigot/mogiplugin/MogiPlugin.java
@@ -0,0 +1,17 @@
+package net.kigawa.minecraft.spigot.mogiplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MogiPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
